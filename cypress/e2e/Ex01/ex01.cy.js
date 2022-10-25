a8ddd5613a77168333fa3ffc8deac019ae43b001
Camila Cassimiro <camilacassimiro@gmail.com>
2022-10-24 21:41:47 -0300

    [M3S08] EX 1 - Cenários CalcSimples

--- cypress/e2e/Ex01/ex01.cy.js
+++ cypress/e2e/Ex01/ex01.cy.js
@@ -14,33 +14,90 @@ context('testCalc', () => {
         cy.get('#txtN2').type(123)
         cy.get('input').contains("Calcular").click()
 
+        cy.get('#lbFormulaCalculo').invoke('text').then(formula => {
+            expect(formula).to.equal('10 + 123')
+        })
+
+        cy.get('#lbResultado').invoke('text').then(result => {
+            expect(result).to.equal('133')
+        })
+
+        cy.get('input').contains('Limpar').click()
+
     });
 
     it('test35-25', () => {
 
         cy.get('#txtN1').type(35)
-        cy.get('#rbFuncao_1')
+        cy.get('#rbFuncao_1').click()
         cy.get('#txtN2').type(25)
         cy.get('input').contains("Calcular").click()
+
+        cy.get('#lbFormulaCalculo').invoke('text').then(formula => {
+            expect(formula).to.equal('35 - 25')
+        })
+
+        cy.get('#lbResultado').invoke('text').then(result => {
+            expect(result).to.equal('10')
+        })
+
+        cy.get('input').contains('Limpar').click()
 
     });
 
     it('test5*55', () => {
 
         cy.get('#txtN1').type(5)
-        cy.get('#rbFuncao_2')
+        cy.get('#rbFuncao_2').click()
         cy.get('#txtN2').type(55)
         cy.get('input').contains("Calcular").click()
+        cy.get('#lbFormulaCalculo').invoke('text').then(formula => {
+            expect(formula).to.equal('5 * 55')
+        })
+
+        cy.get('#lbResultado').invoke('text').then(result => {
+            expect(result).to.equal('275')
+        })
+
+        cy.get('input').contains('Limpar').click()
 
     });
 
     it('test20/2', () => {
 
         cy.get('#txtN1').type(20)
-        cy.get('#rbFuncao_3')
+        cy.get('#rbFuncao_3').click()
         cy.get('#txtN2').type(2)
         cy.get('input').contains("Calcular").click()
 
+        cy.get('#lbFormulaCalculo').invoke('text').then(formula => {
+            expect(formula).to.equal('20 / 2')
+        })
+
+        cy.get('#lbResultado').invoke('text').then(result => {
+            expect(result).to.equal('10')
+        })
+
+        cy.get('input').contains('Limpar').click()
+
     });
 
+    it('calc-45-70', () => {
+        cy.get('#txtN1').type(-45)
+        cy.get('#rbFuncao_1').click()
+        cy.get('#txtN2').type(70)
+
+        cy.get('input').contains('Calcular').click()
+
+        cy.get('#lbFormulaCalculo').invoke('text').then(formula => {
+            expect(formula).to.equal('-45 - 70')
+        })
+
+        cy.get('#lbResultado').invoke('text').then(result => {
+            expect(result).to.equal('-115')
+        })
+        cy.get('input').contains('Limpar').click()
+    });
+
+    
 });
